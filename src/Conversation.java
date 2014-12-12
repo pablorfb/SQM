50d599e1d3bdd723563abe936f18083647e3f3d8
pablorfb <pablorfb@gmail.com>
2014-12-12 13:48:32 +0000

    Protocol initial imlementation

--- src/Conversation.java
+++ src/Conversation.java
@@ -3,11 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Conversation implements Runnable {
-	PrintWriter out;
-	BufferedReader in;
-	Socket clientSocket;
+
+	private static HashMap<String, Conversation> conversations = new HashMap<>();
+	public static final String MESSAGE_SENT = "Message sent.";
+	public static final String ADDRESSE_NOT_FOUND = "Addresee not found.";
+	public static final String SUCCESFULLY_LOGGED_IN = "Succesfully logged in.";
+	public static final String USERNAME_NOT_FOUND = "Username not found.";
+	public static final String SUCCESFULLY_LOGGED_OUT = "Succesfully logged out.";
+	public static final String ALREADY_LOGGED_IN = "User already logged.";
+	
+
+	private PrintWriter out;
+	private BufferedReader in;
+	private Socket clientSocket;
 
 	public Conversation(Socket clientSocket) {
 		this.clientSocket = clientSocket;
@@ -22,6 +36,74 @@ public class Conversation implements Runnable {
 
 	}
 
+	public boolean login(String username) {
+		synchronized (conversations) {
+			if (conversations.get(username) == null) {
+				conversations.put(username, this);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static Set<String> getUsers() {
+		synchronized (conversations) {
+			return conversations.keySet();
+		}
+	}
+	
+	public boolean isLoggedIn(){
+		Set<String> users = Conversation.getUsers();
+		synchronized (conversations) {
+			for (String user : users){
+				if (conversations.get(user) == this){
+					return true;
+				}
+			}
+		}
+		return false;
+				
+	}
+
+	public String sendMessage(String destination, String message) {
+		Conversation adressee;
+		synchronized (conversations) {
+			adressee = conversations.get(destination);
+		}
+		if (adressee == null)
+			return ADDRESSE_NOT_FOUND;
+		adressee.out.write(message);
+		return MESSAGE_SENT;
+	}
+
+	public String sendBroadcast(String message) {
+		Collection<Conversation> addressees;
+		synchronized (conversations) {
+			addressees = conversations.values();
+		}
+		for (Conversation addressee : addressees) {
+			if (addressee != this)
+				addressee.out.write(message);
+		}
+		return MESSAGE_SENT;
+	}
+	
+	public boolean logout() {
+		Set<String> users = Conversation.getUsers();
+		synchronized (conversations) {
+			for (String user : users){
+				if (conversations.get(user) == this){
+					conversations.remove(user);
+					close();
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+
 	public void run() {
 		String userInput;
 		System.out.println("echo: Initialized");
@@ -34,22 +116,25 @@ public class Conversation implements Runnable {
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally { //Close all connections
-			out.close();
-			try {
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					clientSocket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+		} finally { // Close all connections
+			close();
 		}
 
 		System.out.println("End of Conversation");
 	}
 
+	private void close(){
+		out.close();
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				clientSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
